874cc5234cd64908265ae259c18c192dac34d540
hetpatel2004 <het.patel.010420047@gmail.com>
2026-09-03 16:35:38 +0530

    3-Sup-2026

--- frontend/src/pages/SuperAdminStudents.jsx
+++ frontend/src/pages/SuperAdminStudents.jsx
@@ -3,6 +3,7 @@ import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import API from '../api/axios';
 import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Suspense } from 'react';
 import Sidebar from '../components/Sidebar';
 
